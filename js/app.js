815274f8f4276ab372f163c0e48527bd12a3a41c
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-08-24 17:45:55 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(function () {
+	$('#mastheadBanner').fadeIn(2000);
+	$('.tlt').delay(2000).textillate({ in: { effect: 'flash' } });
+	$('#myName').delay(2000).fadeIn(3000);
+	$('#myTitle').delay(4000).fadeIn(3000);
+})
