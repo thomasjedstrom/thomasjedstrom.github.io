7bc2137aabc3264e091e6bef5131055255e51cbb
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-08-31 19:24:33 -0500

    added fabric background and fade out on scroll down

--- js/app.js
+++ js/app.js
@@ -2,4 +2,9 @@ $(function () {
 	$('#mastheadBanner').fadeIn(2000);
 	$('#myName').delay(2000).fadeIn(3000);
 	$('#myTitle').delay(4000).fadeIn(3000);
+
+
+	$(window).scroll(function(){
+		$(".fade-out").css("opacity", 1 - $(window).scrollTop() / 250);
+	});
 })
