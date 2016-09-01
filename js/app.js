77f4afd95e89e019f7ed49c97932c20c804d385b
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-09-01 13:23:41 -0500

    added more parallax, switched over to skrollr completely, started adding new sections

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 $(function () {
+
+	// MASTHEAD fade in effects
+
 	$('#mastheadBanner').fadeIn(2000);
 	$('#myName').delay(2000).fadeIn(3000);
 	$('#myTitle').delay(4000).fadeIn(3000);
 
 
+	// MASTHEAD fade out effects
+
 	$(window).scroll(function(){
-		$(".fade-out").css("opacity", 1 - $(window).scrollTop() / 250);
+		$(".fade-out").css("opacity", 2 - $(window).scrollTop() / 250);
 	});
+
+
+	// SKROLLR initialization
+
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+
+
+
+    // SLIDE IN effects
+ 
 })
