4626962f7a7af7df0b75322e57af5ebeff7b829e
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-09-09 08:28:39 -0500

    made pop ups for profile, music, and contact

--- js/app.js
+++ js/app.js
@@ -58,5 +58,37 @@ $(function () {
 		$('#pianoshadow').fadeOut("slow");
 	})
 
+
+
+
+// POPUPS
+	$(".close").click(function(){
+		contact[0].style.display = "none";
+		portfolio[0].style.display = "none";
+		music[0].style.display = "none";
+	})
+
+	// CONTACT
+	var contact = $('#contact-box');
+	$("#donut").click(function(){
+		contact[0].style.display = "block";
+	})
+	$("#contactword").click(function(){
+		contact[0].style.display = "block";
+	})
+	
+	// PORTFOLIO
+	var portfolio = $("#portfolio-box")
+	$("#briefcaseclicker").click(function(){
+		portfolio[0].style.display = "block";
+	})
+
+	// MUSIC
+	var music = $("#music-box")
+	$("#pianoclicker").click(function(){
+		music[0].style.display = "block";
+	})	
+
+
 })
 
