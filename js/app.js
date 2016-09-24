cb376a6eec2a296b8e37195c662572dd4064ab42
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-09-23 23:28:02 -0500

    improved js readability

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ $(function () {
 			if (window.addEventListener){
 				window.addEventListener('DOMMouseScroll', preventDefault, false);
 			}
-			window.onwheel = preventDefault;
+			window.onwheel 		= preventDefault;
 			window.onmousewheel = document.onmousewheel = preventDefault;
 			window.ontouchmove  = preventDefault;
 			document.onkeydown  = preventDefaultForScrollKeys;
@@ -40,22 +40,11 @@ $(function () {
 
 		// BUTTONS TO SCROLL
 
-		$('#enter_here').click(function(){
-			$(window).scrollTo('50%', 1300);	
-		})
-		$('#back_to_landing').click(function(){
-			$(window).scrollTo('0%', 1300);	
-		})
-		$('#to_connections').click(function(){
-			$(window).scrollTo('100%', 1300);	
-		})
-		$('#to_connections').click(function(){
-			$(window).scrollTo('100%', 1300);	
-		})
-		$('#back_to_bio').click(function(){
-			$(window).scrollTo('50%', 1300);	
-		})
-
+		$('#enter_here')		.click(()=>{$(window).scrollTo('50%', 1300)});	
+		$('#back_to_landing')	.click(()=>{$(window).scrollTo('0%', 1300)});	
+		$('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});	
+		$('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});
+		$('#back_to_bio')		.click(()=>{$(window).scrollTo('50%', 1300)});			
 
 		// SKROLLR initialization
 
@@ -69,91 +58,63 @@ $(function () {
 
 	// MASTHEAD fade in effects
 
-	$('#mastheadBanner').fadeIn(2000).css("display", "inline-block");
-	$('#myName').css('display', 'inline-block').hide().delay(1500).fadeIn(3000);
-	$('#myTitle').css('display', 'inline-block').hide().delay(2500).fadeIn(3000);
+	$('#mastheadBanner')	.fadeIn(2000).css("display", "inline-block");
+	$('#myName')			.css('display', 'inline-block').hide().delay(1500).fadeIn(3000);
+	$('#myTitle')			.css('display', 'inline-block').hide().delay(2500).fadeIn(3000);
 
 
 
 	// MASTHEAD fade out effects
 
 	$(window).scroll(function(){
-		$(".fade-out").css("opacity", 2 - $(window).scrollTop() / 250);
+		$(".fade-out")		.css("opacity", 2 - $(window).scrollTop() / 250);
 	});
 
 
 
 	// LINKS CLICKABLE AREA
 
-    $('#newspaperclicker').hover(function(){
-		$('#newspapershadow').fadeIn("slow");
-	})
-    $('#newspaperclicker').mouseout(function(){
-		$('#newspapershadow').fadeOut("slow");
-	})
+    $('#newspaperclicker')	.hover(()=>{$('#newspapershadow').fadeIn("slow")})
+    $('#newspaperclicker')	.mouseout(()=>{$('#newspapershadow').fadeOut("slow")})
 
-	$('#suitclicker').hover(function(){
-		$('#suitshadow').fadeIn("slow");
-	})	
-	$('#suitclicker').mouseout(function(){
-		$('#suitshadow').fadeOut("slow");
-	})
+	$('#suitclicker')		.hover(()=>{$('#suitshadow').fadeIn("slow")})	
+	$('#suitclicker')		.mouseout(()=>{$('#suitshadow').fadeOut("slow")})
 
-	$('#briefcaseclicker').hover(function(){
-		$('#briefcaseshadow').fadeIn("slow");
-	})	
-	$('#briefcaseclicker').mouseout(function(){
-		$('#briefcaseshadow').fadeOut("slow");
-	})
+	$('#briefcaseclicker')	.hover(()=>{$('#briefcaseshadow').fadeIn("slow")})	
+	$('#briefcaseclicker')	.mouseout(()=>{$('#briefcaseshadow').fadeOut("slow")})
 
-	$('#octocatclicker').hover(function(){
-		$('#octocatshadow').fadeIn("slow");
-	})	
-	$('#octocatclicker').mouseout(function(){
-		$('#octocatshadow').fadeOut("slow");
-	})
+	$('#octocatclicker')	.hover(()=>{$('#octocatshadow').fadeIn("slow")})	
+	$('#octocatclicker')	.mouseout(()=>{$('#octocatshadow').fadeOut("slow")})
 
-	$('#pianoclicker').hover(function(){
-		$('#pianoshadow').fadeIn("slow");
-	})	
-	$('#pianoclicker').mouseout(function(){
-		$('#pianoshadow').fadeOut("slow");
-	})
+	$('#pianoclicker')		.hover(()=>{$('#pianoshadow').fadeIn("slow")})	
+	$('#pianoclicker')		.mouseout(()=>{$('#pianoshadow').fadeOut("slow")})
 
 
 
 
 // POPUPS
 	$(".close").click(function(){
-		contact[0].style.display = "none";
-		portfolio[0].style.display = "none";
-		music[0].style.display = "none";
+		contact[0]			.style.display = "none";
+		portfolio[0]		.style.display = "none";
+		music[0]			.style.display = "none";
 
-		portfolioMobile[0].style.display = "none";
-		musicMobile[0].style.display = "none";
-		contactMe[0].style.display = "none";
+		portfolioMobile[0]	.style.display = "none";
+		musicMobile[0]		.style.display = "none";
+		contactMe[0]		.style.display = "none";
 	})
 
 	// CONTACT
 	var contact = $('#contact-box');
-	$("#donut").click(function(){
-		contact[0].style.display = "block";
-	})
-	$("#contactword").click(function(){
-		contact[0].style.display = "block";
-	})
+	$("#donut")				.click(()=>{contact[0].style.display = "block"})
+	$("#contactword")		.click(()=>{contact[0].style.display = "block"})
 	
 	// PORTFOLIO
 	var portfolio = $("#portfolio-box")
-	$("#briefcaseclicker").click(function(){
-		portfolio[0].style.display = "block";
-	})
+	$("#briefcaseclicker")	.click(()=>{portfolio[0].style.display = "block"})
 
 	// MUSIC
 	var music = $("#music-box")
-	$("#pianoclicker").click(function(){
-		music[0].style.display = "block";
-	})	
+	$("#pianoclicker")		.click(()=>{music[0].style.display = "block"})	
 
 
 
@@ -165,9 +126,9 @@ $(function () {
 
 	// MASTHEAD fade in effects
 
-	$('#landingBanner').fadeIn(2000).css("display", "inline-block");
-	$('#name').css('display', 'inline-block').hide().delay(1500).fadeIn(3000);
-	$('#title').css('display', 'inline-block').hide().delay(2500).fadeIn(3000);
+	$('#landingBanner')		.fadeIn(2000).css("display", "inline-block");
+	$('#name')				.css('display', 'inline-block').hide().delay(1500).fadeIn(3000);
+	$('#title')				.css('display', 'inline-block').hide().delay(2500).fadeIn(3000);
 
 
 
@@ -175,25 +136,21 @@ $(function () {
 	
 	// PORTFOLIO
 	var portfolioMobile = $("#portfolio-boxMobile")
-	$("#portfolioClickerMobile").click(function(){
-		portfolioMobile[0].style.display = "block";
-	})
+	$("#portfolioClickerMobile")	.click(()=>{portfolioMobile[0].style.display = "block"})
 
 	// MUSIC
 	var musicMobile = $("#music-boxMobile")
-	$("#musicClickerMobile").click(function(){
-		musicMobile[0].style.display = "block";
-	})
+	$("#musicClickerMobile")		.click(()=>{musicMobile[0].style.display = "block"})
 
 	// CONTACT
 	var contactMe = $('#contact-boxMobile');
-	$("#contactMe").click(function(){
-		contactMe[0].style.display = "block";
-	})
-	$(".sendMeALine").click(function(){
-		contact[0].style.display = "block";
-		contactMe[0].style.display = "block";
-	})
+	$("#contactMe")					.click(()=>{contactMe[0].style.display = "block"})
+	$(".sendMeALine")				.click(
+		()=>{
+			contact[0].style.display = "block";
+			contactMe[0].style.display = "block";
+		}
+	)
 
 
 })
