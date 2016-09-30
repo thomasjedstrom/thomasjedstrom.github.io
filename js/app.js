26f64f83f6a8540856029f7e8a72a682088f2701
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-09-29 21:04:15 -0500

    started a new branch to refactor and optimize

--- js/app.js
+++ js/app.js
@@ -1,12 +1,7 @@
 $(function () {
 
-
-	// LOADING PAGE
-	setTimeout(function(){
-		$('body').addClass('loaded');
-	}, 1000);
-
-
+	// PARALLAX
+	// $('.parallax-window').parallax({imageSrc: './img/masthead_background.jpg'});
 
 	// MEDIA QUERY FOR SKROLLR AND SCROLLING
 	var mq = window.matchMedia( "(min-width: 1025px)" );
@@ -17,61 +12,119 @@ $(function () {
 			location.reload();
 		});		
 
-		// PREVENT SCROLLING
-		var keys = {37: 1, 38: 1, 39: 1, 40: 1};
-
-		function preventDefault(e) {
-			e = e || window.event;
-			if (e.preventDefault){
-				e.preventDefault();
-				e.returnValue = false;
-			}  
-		}
-		function preventDefaultForScrollKeys(e) {
-			if (keys[e.keyCode]) {
-				preventDefault(e);
-				return false;
-			}
-		}
-		function disableScroll() {
-			if (window.addEventListener){
-				window.addEventListener('DOMMouseScroll', preventDefault, false);
-			}
-			window.onwheel 		= preventDefault;
-			window.onmousewheel = document.onmousewheel = preventDefault;
-			window.ontouchmove  = preventDefault;
-			document.onkeydown  = preventDefaultForScrollKeys;
-		}
-
-		// disableScroll();
-
 		// BUTTONS TO SCROLL
 
-		$('#enter_here')		.click(()=>{$(window).scrollTo('50%', 1300)});	
-		$('#back_to_landing')	.click(()=>{$(window).scrollTo('0%', 1300)});	
-		$('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});	
-		$('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});
-		$('#back_to_bio')		.click(()=>{$(window).scrollTo('50%', 1300)});			
+		// $('#enter_here')		.click(()=>{$(window).scrollTo('50%', 1300)});	
+		// $('#back_to_landing')	.click(()=>{$(window).scrollTo('0%', 1300)});	
+		// $('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});	
+		// $('#to_connections')	.click(()=>{$(window).scrollTo('100%', 1300)});
+		// $('#back_to_bio')		.click(()=>{$(window).scrollTo('50%', 1300)});			
 
-		// SKROLLR initialization
+		// // SKROLLR initialization
 
-	    var s = skrollr.init({
-	        render: function(data) {
-	            //Debugging - Log the current scroll position.
-	            //console.log(data.curTop);
-	        }
-	    });		
+		// var s = skrollr.init({
+		// 	render: function(data) {
+		// 		//Debugging - Log the current scroll position.
+		// 		console.log(data.curTop);
+		// 	}
+		// });	
 	}
 
-	// MASTHEAD fade in effects
 
-	$('#mastheadBanner')	.fadeIn(2000).css("display", "inline-block");
+
+
+
+
+
+
+
+
+
+
+	// ANIMATIONS
+
+
+
+
+	// variables
+	var $isAnimatedSecond = $('.is-animated'),
+		$isAnimatedSecondSingle = $('.second .is-animated__single');
+
+	var $curtainL = $('#curtain1'),
+		$curtainR = $('#curtain2'),
+		$bgHanging = $('#bgHanging'),
+		$fader = $('.fader');
+
+	// initialize fullPage
+	$('#fullpage').fullpage({
+
+		// navigation: true,
+		css3:false,
+		onLeave: function(index, nextIndex, direction) {
+			// page2 - page3
+			if( index == 2 && nextIndex == 3 ) {
+				$curtainL.animate({'left': "-=50vw"}, "slow");
+				$curtainR.animate({'left': "+=50vw"}, "slow");
+				$fader.fadeOut('slow');
+				// $bgHanging.addClass('animated slideInDown');
+
+			// Page 3 - Page 2
+			}else if( index == 3 && direction == 'up') {
+				$curtainL.animate({'left': "+=50vw"}, "slow");
+				$curtainR.animate({'left': "-=50vw"}, "slow");
+				$fader.fadeIn('slow');
+			}
+
+			// second animation
+			// else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
+			// 	$isAnimatedThird.eq(0).addClass('animated fadeInRightBig').css('animation-delay', '.3s'); 
+			// 	$isAnimatedThird.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
+			// 	$isAnimatedThirdSingle.addClass('animated bounceInDown').css('animation-delay', '1.2s');
+			// }
+
+			// third animation
+			// else if( ( index == 1 || index == 2 || index == 3 ) && nextIndex == 4 ) {
+			// 	$isAnimatedFourth.addClass('animated zoomIn').css('animation-delay', '.6s');
+			// 	$isAnimatedFourthSingle.addClass('animated lightSpeedIn').css('animation-delay', '1.2s');
+			// 	$isAnimatedFourthSingle.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+			// 		$(this).removeClass('lightSpeedIn').addClass('zoomOutDown');
+				// $isAnimatedSecond.addClass('animated fadeInUpBig'); 
+				// $isAnimatedSecond.eq(0).css('animation-delay', '.3s');
+				// $isAnimatedSecond.eq(1).css('animation-delay', '.6s');
+				// $isAnimatedSecond.eq(2).css('animation-delay', '.9s');
+				// $isAnimatedSecondSingle.addClass('animated rollIn').css('animation-delay', '1.7s');
+
+			// 	});
+			// }
+		}
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// LOADING PAGE
+	setTimeout(function(){
+		$('body').addClass('loaded');
+	}, 1000);
+
+	// firstPage fade in effects
+
+	$('#firstPageBanner')	.fadeIn(2000).css("display", "inline-block");
 	$('#myName')			.css('display', 'inline-block').hide().delay(1500).fadeIn(3000);
 	$('#myTitle')			.css('display', 'inline-block').hide().delay(2500).fadeIn(3000);
 
-
-
-	// MASTHEAD fade out effects
+	// firstPage fade out effects
 
 	$(window).scroll(function(){
 		$(".fade-out")		.css("opacity", 2 - $(window).scrollTop() / 250);
@@ -81,8 +134,8 @@ $(function () {
 
 	// LINKS CLICKABLE AREA
 
-    $('#newspaperclicker')	.hover(()=>{$('#newspapershadow').fadeIn("slow")})
-    $('#newspaperclicker')	.mouseout(()=>{$('#newspapershadow').fadeOut("slow")})
+	$('#newspaperclicker')	.hover(()=>{$('#newspapershadow').fadeIn("slow")})
+	$('#newspaperclicker')	.mouseout(()=>{$('#newspapershadow').fadeOut("slow")})
 
 	$('#suitclicker')		.hover(()=>{$('#suitshadow').fadeIn("slow")})	
 	$('#suitclicker')		.mouseout(()=>{$('#suitshadow').fadeOut("slow")})
